4083d3095391296cb3eefe539df6475200f54e66
Stacey Sern <stacey@dropbox.com>
2019-12-03 15:33:45 -0500

    Publishable

--- cli/peerdep-paths.js
+++ cli/peerdep-paths.js
@@ -1,7 +1,7 @@
 
 try {
     // installed as a peer dependency
-    exports.pathToProtobufJs = require.resolve("protobufjs")
+    exports.pathToProtobufJs = require.resolve("protobufjs-stacey")
 } catch (e) {
     // local development, i.e. forked from github
     exports.pathToProtobufJs = require.resolve("..")
